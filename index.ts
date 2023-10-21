2ea19d375e414f4fb1a7f0d9a293ea39e405e618
Charlie Appleton <xjarlie@gmail.com>
2023-10-21 23:24:55 +0900

    messin

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
-import express from "express";
+import express from 'express';
 import path from "path";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import router from "./routes/router";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
+app.set('view engine', 'ejs');
 
+app.use('/', router);
+
+app.get('/hello', (req, res) => {
+    res.json({text: 'hello'});
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
