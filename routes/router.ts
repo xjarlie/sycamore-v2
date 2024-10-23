b74c913927998d485797cd934f01c9a0c7998789
Charlie Appleton <xjarlie@gmail.com>
2024-10-23 19:44:24 +0100

    add prefilled server url to signup page

--- routes/router.ts
+++ routes/router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import clientRouter from './client';
 import serverRouter from './server';
 import { verifyToken } from '../lib/authTokens';
+import { serverInfo } from '../lib/serverInfo';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.use('/syc/client', verifyToken, clientRouter);
 router.use('/syc/server', serverRouter);
 
 router.get('/signup', (req, res) => {
-    res.render('signup');
+    res.render('signup', { serverUrl: serverInfo.address });
 })
 
 router.get('/', (req, res) => {
